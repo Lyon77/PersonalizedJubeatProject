44872e22b1f98907163adab5a981a1eebc134f85
Lyon77 <rjl7799@gmail.com>
2019-10-13 14:51:18 -0400

    Added New Scene for Selecting Songs
    This new scene is allows for endless scrolling for songs. I need to add the ability for the Songs to be grabbed from the Songs folder, but for now, it is staticly created. I'm also implementating songs to now be heard in the Main scene. However, the delay doesn't seem to be working. Might be an issue with the current Unity version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject box;
+    public AudioSource music;
+    private bool stopped;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,14 +28,37 @@ public class GameManager : MonoBehaviour
         //Since we have a even number of cubes, we need to shift by half the distance
         for (int i = -2; i <= 1; i++) {
             for (int j = -2; j <= 1; j++) {
-                Instantiate(box, new Vector3(i * distance + distance / 2, j * distance + distance / 2), Quaternion.identity);
+                Instantiate(box, new Vector3(i * distance + distance / 2, j * distance + distance / 2 - 1), Quaternion.identity);
             }
         }
-    }   
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        music = GetComponent<AudioSource>();
+        StartCoroutine("PlayMusic", 10);
+        Debug.Log("Music Starts in 3 Seconds");
+
+        stopped = false;
+    }
+
+    IEnumerator PlayMusic(float Count) {
+        yield return new WaitForSeconds(Count); //Count is the amount of time in seconds that you want to wait.
+
+        //PlayDelayed() didn't work
+        music.Play();
+
+        yield return null;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("p")) {
+            if (stopped) {
+                music.UnPause();
+                stopped = false;
+                Debug.Log("Music Unpaused");
+            } else {
+                music.Pause();
+                stopped = true;
+                Debug.Log("Music Paused");
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ScrollRectSnap.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSnap : MonoBehaviour
+{
+    public RectTransform panel; //ScrollPanel
+    public RectTransform center;
+
+    private Button[] bttn;
+    private float[] distance;
+    private float[] distReposition;
+    private bool dragging = false;
+    private int bttnDistance;
+    private int minButtonNum;
+    
+
+    void Start() {
+        //this is to get the Button list because the amount of buttons is unknown
+        bttn = panel.GetComponentsInChildren<Button>();
+
+        int bttnLength = bttn.Length;
+        distance = new float[bttnLength];
+        distReposition = new float[bttnLength];
+
+        //Get Distance between buttons
+        bttnDistance = (int)Mathf.Abs(bttn[1].GetComponent<RectTransform>().anchoredPosition.x - bttn[0].GetComponent<RectTransform>().anchoredPosition.x);
+    }
+
+    void Update() {
+        for (int i = 0; i < bttn.Length; i++) {
+            distReposition[i] = center.GetComponent<RectTransform>().position.x - bttn[i].GetComponent<RectTransform>().position.x;
+            distance[i] = Mathf.Abs(distReposition[i]);
+
+            if (distReposition[i] > 11f / 5f * bttn.Length) {
+                float curX = bttn[i].GetComponent<RectTransform>().anchoredPosition.x;
+                float curY = bttn[i].GetComponent<RectTransform>().anchoredPosition.y;
+
+                //loop buttons
+                Vector2 newAnchor = new Vector2(curX + bttn.Length * bttnDistance, curY);
+                bttn[i].GetComponent<RectTransform>().anchoredPosition = newAnchor;
+            }
+
+            if (distReposition[i] < -11f / 5f * bttn.Length) {
+                float curX = bttn[i].GetComponent<RectTransform>().anchoredPosition.x;
+                float curY = bttn[i].GetComponent<RectTransform>().anchoredPosition.y;
+
+                //loop buttons
+                Vector2 newAnchor = new Vector2(curX - bttn.Length * bttnDistance, curY);
+                bttn[i].GetComponent<RectTransform>().anchoredPosition = newAnchor;
+            }
+        }
+
+        float minDistance = Mathf.Min(distance);
+
+        for (int i = 0; i < bttn.Length; i++) {
+            if (minDistance == distance[i]) {
+                minButtonNum = i;
+                break;
+            }
+        }
+
+        //check if we are dragging
+        if (!dragging) {
+            //MoveToButton(minButtonNum * -bttnDistance);
+            MoveToButton(-bttn[minButtonNum].GetComponent<RectTransform>().anchoredPosition.x);
+        }
+    }
+
+    private void MoveToButton(float position) {
+        float newX = Mathf.Lerp(panel.anchoredPosition.x, position, Time.deltaTime * 10f);
+        Vector2 newPos = new Vector2(newX, panel.anchoredPosition.y);
+
+        panel.anchoredPosition = newPos;
+    }
+
+    public void StartDrag() {
+        dragging = true;
+    }
+
+    public void EndDrag() {
+        dragging = false;
+    }
+}
